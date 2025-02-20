43e51a76ba53523c0d4aef129efb056873968305
josmary <josmarypirela1712@outlook.com>
2025-02-20 15:30:27 -0400

    finish button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+
+import Button from "../../Components/Button/Button";
+import "./Home.css"
+
 const HomeComponent = () => {
   return (
     <>
       <p>Hola</p>
+      <Button text="Ir a Contacto" />
     </>
   );
 };
